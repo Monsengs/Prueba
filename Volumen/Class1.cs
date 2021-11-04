a1de5cdda065dec7ebf5ddc6be19c470306afb90
Monsengs <93671739+Monsengs@users.noreply.github.com>
2021-11-03 18:26:59 -0600

    si se pudo

---
+++ Volumen/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volumen
+{
+    class Class1
+    {
+        int[] volumen;
+        int altura, longitud, ancho, total, i, final;
+
+        public void getVolumen()
+        {
+            volumen = new int[2];
+            for (i = 0; i < volumen.Length; i++)
+            {
+                Console.WriteLine("Ingrese la altura: ");
+                altura = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese la longitud: ");
+                longitud = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese el ancho: ");
+                ancho = int.Parse(Console.ReadLine());
+
+                total = altura * longitud * ancho;
+
+                Console.WriteLine("El volumen es " + total);
+
+                final += total;
+            }
+            suma(final);
+        }
+
+        public string suma(int final)
+        {
+            string frase = "El volumen de la tercera figura es " + final;
+            Console.WriteLine("El volumen de la tercera figura es " + final);
+            return frase;
+        }
+    }
+}
---
+++ Volumen/Volumen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Volumen
+{
+    public class Volumen
+    {
+        public decimal Altura { get; set; }
+        public decimal Longitud { get; set; }
+        public decimal Ancho { get; set; }
+
+        public decimal getVolumen()
+        {
+            return Altura * Longitud * Ancho;
+        }
+
+        public static Volumen getVolumen(Volumen prisma1, Volumen prisma2)
+        {
+            Volumen prisma3 = new Volumen();
+            prisma3.Altura = prisma1.Altura + prisma2.Altura;
+            prisma3.Ancho = prisma1.Ancho + prisma2.Ancho;
+            prisma3.Longitud = prisma1.Longitud + prisma2.Longitud;
+            return prisma3;
+        }
+
+        public static void Main(string[] args)
+        {
+            Volumen prisma1 = new Volumen();
+            prisma1.Altura = 5;
+            prisma1.Longitud = 3;
+            prisma1.Ancho = 2;
+            Console.WriteLine(prisma1.getVolumen());
+
+            Console.WriteLine("-----------------------------------------");
+
+            Volumen prisma2 = new Volumen();
+            prisma2.Altura = 4;
+            prisma2.Longitud = 2;
+            prisma2.Ancho = 4;
+            Console.WriteLine(prisma2.getVolumen());
+
+            Console.WriteLine("-----------------------------------------");
+
+            
+            Volumen prisma_3 = getVolumen(prisma1, prisma2);
+            Console.WriteLine(prisma_3.getVolumen());
+            
+
+            Console.ReadLine();
+            
+        }
+    }
+}
